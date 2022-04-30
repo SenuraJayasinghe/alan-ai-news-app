cd67010af3e646dbab1868698ea066ad79088b93
senurajayasinghe.se@gmail.com <senurajayasinghe.se@gmail.com>
2022-04-30 20:46:18 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import wordsToNumbers from 'words-to-numbers'
+import NewsCards from './components/NewsCards/NewsCards';
 
-function App() {
+
+const alanKey = 'fb71fbef48ffae072328d34a56f45c3d2e956eca572e1d8b807a3e2338fdd0dc/stage';
+
+const App =() => {
+  const [newsArticles, setNewsArticles] = useState([]);
+   const [activeArticle, setActiveArticle] = useState([-1]);
+  useEffect(() => {
+    function updateScreen(time) {
+        // Make visual updates here.
+        alanBtn({
+            key: alanKey,
+            onCommand: ({ command, articles, number }) => {
+                if (command === 'newHeadLines') {
+                  setNewsArticles (articles);
+                   setActiveArticle(-1);
+                 
+                }
+                 else if(command === 'highlight') {
+                     setActiveArticle((prevActiveArticle)=> prevActiveArticle + 1 );
+                }
+                else if(command === 'open') {
+                 const parsedNumber = number.length > 2 ? wordsToNumbers(number, {fuzzy: true}) : number;
+                 const article = articles[parsedNumber - 1];
+
+                 if (parsedNumber > articles.length) {
+                  alanBtn.playText('Please try that again...');
+                } else if (article) {
+                  window.open(article.url, '_blank');
+                  alanBtn.playText('Opening...');
+                } else {
+                  alanBtn.playText('Please try that again...');
+                
+                 }
+                }
+            }
+        })
+    }
+
+    requestAnimationFrame(updateScreen);
+}, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-img'>
+    <div className='heading'>
+     <h1>Alan AI Web App Integration</h1>
     </div>
-  );
+    {/* <div className="logoContainer" >
+      <img src="https://images.pexels.com/photos/2235130/pexels-photo-2235130.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="alanLogo" alt="logo" />
+    </div> */}
+    <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+    </div>
+    
+  )
 }
 
 export default App;
+
+//  line 39
---
+++ src/components/NewsCard/styles.js
@@ -0,0 +1,10 @@
+import { makeStyles} from '@mui/styles'; 
+
+export default makeStyles({
+    media: {
+      height: 250,
+    },
+    card: {
+        color: 'blue',
+    }
+})
